d2f67b117b376e30d29895330789630c59261a3a
ejj28 <eriksinventions11@gmail.com>
2018-10-25 07:33:33 -0400

    Fixed noise issues, scalable rendering works

--- core.py
+++ core.py
@@ -4,6 +4,9 @@ from PIL import Image, ImageDraw
 counter = 7
 counter2 = 22
 counter3 = 7
+width = 700
+height = 400
+
 files = [
   '/Users/Erik/localCoding/windowgen/assets/upperrightcorner.png',
   '/Users/Erik/localCoding/windowgen/assets/bottomrightcorner.png',
@@ -16,7 +19,7 @@ files = [
   '/Users/Erik/localCoding/windowgen/assets/rightedge.png'
   ]
   
-result = Image.new("RGB", (50, 50))
+result = Image.new("RGB", (width, height))
 img0 = Image.open(files[4])
 img1 = Image.open(files[2])
 img2 = Image.open(files[3])
@@ -31,24 +34,26 @@ w1, h1 = img1.size
 w2, h2 = img2.size
 w3, h3 = img3.size
 w4, h4 = img4.size
-result.paste(img0, (0, 0))
+dr = ImageDraw.Draw(result)
+dr.rectangle([0,0,width,height],fill="blue")
+#result.paste(img0, (0, 0))
 result.paste(img1, (0, 0), img1)
 
-for i in range(1,35):
+for i in range(1,((width - 6) - 7) - 1):
     result.paste(img5, (counter, 0))
     
     counter += 1
-for i in range (1,15):
+for i in range (1,((height - 22) - 7)):
     result.paste(img8, (counter, counter2))
     result.paste(img7, (0, counter2))
     counter2 += 1
-for i in range(1,35):
+for i in range(1,((width - 6) - 7) - 1):
     
     result.paste(img6, (counter3, counter2))
     counter3 += 1
 result.paste(img3, (counter, 0), img3)
 result.paste(img4, (counter, counter2), img4)
 result.paste(img2, (0, counter2), img2)
-dr = ImageDraw.Draw(result)
+
 dr.rectangle([7,22,counter,counter2],fill="#dedede")
-result.save('/Users/Erik/localCoding/windowgen/image.jpg', dither=Image.NONE)
+result.save('/Users/Erik/localCoding/windowgen/image.png')
