92b306442afa32d9d3860380ea515277a4cfa5cc
ejj28 <eriksinventions11@gmail.com>
2019-02-13 23:15:12 -0500

    Changed variable names to prevent insanity

--- core.py
+++ core.py
@@ -20,15 +20,15 @@ files = [
   ]
   
 result = Image.new("RGB", (width, height))
-img0 = Image.open(files[4])
-img1 = Image.open(files[2])
-img2 = Image.open(files[3])
-img3 = Image.open(files[0])
-img4 = Image.open(files[1])
-img5 = Image.open(files[5])
-img6 = Image.open(files[6])
-img7 = Image.open(files[7])
-img8 = Image.open(files[8])
+bg = Image.open(files[4])
+ulc = Image.open(files[2])
+blc = Image.open(files[3])
+urc = Image.open(files[0])
+brc = Image.open(files[1])
+te = Image.open(files[5])
+be = Image.open(files[6])
+le = Image.open(files[7])
+re = Image.open(files[8])
 
 w1, h1 = img1.size
 w2, h2 = img2.size
@@ -36,24 +36,24 @@ w3, h3 = img3.size
 w4, h4 = img4.size
 dr = ImageDraw.Draw(result)
 dr.rectangle([0,0,width,height],fill="blue")
-#result.paste(img0, (0, 0))
-result.paste(img1, (0, 0), img1)
+#result.paste(bg, (0, 0))
+result.paste(ulc, (0, 0), ulc)
 
 for i in range(1,((width - 6) - 7) - 1):
-    result.paste(img5, (counter, 0))
+    result.paste(tb, (counter, 0))
     
     counter += 1
 for i in range (1,((height - 22) - 7)):
-    result.paste(img8, (counter, counter2))
-    result.paste(img7, (0, counter2))
+    result.paste(re, (counter, counter2))
+    result.paste(le, (0, counter2))
     counter2 += 1
 for i in range(1,((width - 6) - 7) - 1):
     
-    result.paste(img6, (counter3, counter2))
+    result.paste(te, (counter3, counter2))
     counter3 += 1
-result.paste(img3, (counter, 0), img3)
-result.paste(img4, (counter, counter2), img4)
-result.paste(img2, (0, counter2), img2)
+result.paste(urc, (counter, 0), urc)
+result.paste(brc, (counter, counter2), brc)
+result.paste(blc, (0, counter2), blc)
 
 dr.rectangle([7,22,counter,counter2],fill="#dedede")
 result.save('image.png')
